a483f183103be2dca378769d72fe28d593c9f2f8
Rob Anderson <robanderson@quickenloans.com>
2015-07-24 15:35:28 -0400

    More improvements

--- resources/js/main.js
+++ resources/js/main.js
@@ -6,16 +6,30 @@ $(document).ready(function () {
   });
   $('a[href*=#]:not([href=#])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var duration = 250;
       var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      var setTarget = $('[name=' + this.hash.slice(1) +']');
+      var theTarget = $('#' + this.hash.slice(1)).offset().top;
+
+      if (theTarget > 750) {
+          duration = 500;
+      } else if (theTarget > 1500) {
+          duration = 1000;
+      }
+
+      target = target.length ? target : setTarget;
+
       if (target.length) {
         var offset = ($('nav').outerHeight());
+
         if ($('nav').hasClass('fixed-nav') === false) {
             offset = offset + 64;
         }
+
         $('html,body').animate({
             scrollTop: (target.offset().top - offset)
-        }, 500);
+        }, duration);
+
         return false;
       }
     }
